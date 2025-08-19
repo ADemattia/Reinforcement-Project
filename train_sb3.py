a19929542ed8bb41795409d709bd51b91ba00d22
ADemattia <127196391+ADemattia@users.noreply.github.com>
2025-08-19 18:20:44 +0200

    Update train_sb3.py

--- train_sb3.py
+++ train_sb3.py
@@ -4,7 +4,8 @@
     Read the stable-baselines3 documentation and implement a training
     pipeline with an RL algorithm of your choice between PPO and SAC.
 """
-import gym
+import gymnasium as gym
+
 from env.custom_hopper import *
 
 def main():
